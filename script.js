32b3d137eecbb992c5e77b12b865aec0b75eca87
PauSG093 <138589778+PauSG093@users.noreply.github.com>
2024-01-18 19:51:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function cambiarColorFondo() {
+    var segundoDiv = document.getElementById('efectosMovimiento');
+
+    segundoDiv.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
